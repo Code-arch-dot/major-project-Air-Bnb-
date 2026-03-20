b3ff43dbbf4f78eae184eb3425b1993e32e7753c
Code-arch-dot <ratneshchaudhary44@gmail.com>
2026-03-21 04:09:40 +0530

    updated code

--- app.js
+++ app.js
@@ -152,6 +152,9 @@ app.use((err, req, res, next) => {
 // app.get("/", (req, res) => {
 //   res.send("server is working");
 // });
+app.get("/", (req, res) => {
+  res.render("listings/index.ejs");
+});
 app.listen(3000, () => {
   console.log("server is listening at port 3000");
 });
